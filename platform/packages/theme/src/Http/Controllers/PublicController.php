73dcf2b84f9e629eb1ff891a411b8db2894bdcdf
mo2men <momen.elsyd@gmail.com>
2025-08-13 00:22:58 +0300

    update graph image

--- platform/packages/theme/src/Http/Controllers/PublicController.php
+++ platform/packages/theme/src/Http/Controllers/PublicController.php
@@ -23,6 +23,7 @@ class PublicController extends BaseController
     public function getIndex()
     {
         Theme::addBodyAttributes(['id' => 'page-home']);
+        SeoHelper::setImage("https://pub-a5cbc67795e5406587a33b0473a3d19b.r2.dev/general/open-graph-image-1.png");
 
         if (defined('PAGE_MODULE_SCREEN_NAME') && BaseHelper::getHomepageId()) {
             $data = (new PageService())->handleFrontRoutes(null);
