edc5dbcfd0bef2a94e0267eff565ea19a633e76c
mo2men <momen.elsyd@gmail.com>
2025-08-18 22:07:10 +0300

    init google analytics

--- platform/packages/installer/resources/views/layouts/master.blade.php
+++ platform/packages/installer/resources/views/layouts/master.blade.php
@@ -74,6 +74,14 @@
     >
 
     @yield('styles')
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9DPJ55NCXD"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-9DPJ55NCXD');
+    </script>
 </head>
 
 <body>
